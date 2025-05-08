97643b4cb866ff26d0a44bdc78d8fe67cf3927c5
art1factia <137972596+art1factia@users.noreply.github.com>
2025-05-08 15:37:52 +0900

    init

--- src/main/java/likelion13th/blog/BlogApplication.java
+++ src/main/java/likelion13th/blog/BlogApplication.java
@@ -3,7 +3,6 @@ package likelion13th.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class BlogApplication {
 
