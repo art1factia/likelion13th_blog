1d5a9c595f7300129d39cf3b00fd6da4107b3a80
art1factia <137972596+art1factia@users.noreply.github.com>
2025-05-26 20:23:31 +0900

    Feat: 댓글 domain 코드 추가

--- src/main/java/likelion13th/blog/domain/Comment.java
+++ src/main/java/likelion13th/blog/domain/Comment.java
@@ -1,51 +1,36 @@
-//package likelion13th.blog.domain;
-//
-//import java.time.LocalDateTime;
-//
-//public class Comment {
-//    private Long id;
-//    private Article article;
-//    private String content;
-//    private String author;
-//    private String password;
-//    private LocalDateTime createdAt;
-//
-//    public Comment(Long id, String author, String password, Article article, String content){
-//        this.id = id;
-//        this.content = content;
-//        this.author = author;
-//        this.password = password;
-//        this.createdAt = LocalDateTime.now();
-//        this.article = article;
-//    }
-//
-//    public Article getArticle() {
-//        return article;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public LocalDateTime getCreatedAt() {
-//        return createdAt;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//
-//
-//
-//
-//
-//}
+package likelion13th.blog.domain;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="article_id", nullable = false)
+    @OnDelete(action=OnDeleteAction.CASCADE)
+    private Article article;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private Long password;
+
+    @Column(nullable = false)
+    private Long author;
+
+}
