b4969490266f97d599695abcf53566b48569de91
art1factia <137972596+art1factia@users.noreply.github.com>
2025-05-26 18:30:07 +0900

    Feat: JPA를 사용한 코드 리팩토링

--- src/main/java/likelion13th/blog/service/ArticleService.java
+++ src/main/java/likelion13th/blog/service/ArticleService.java
@@ -1,51 +1,48 @@
 package likelion13th.blog.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import likelion13th.blog.domain.Article;
+import likelion13th.blog.dto.AddArticleRequest;
+import likelion13th.blog.dto.ArticleResponse;
+import likelion13th.blog.dto.SimpleArticleResponse;
+import likelion13th.blog.repository.ArticleRepository;
+import lombok.*;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
+@RequiredArgsConstructor
 public class ArticleService {
-    private final List<Article> articleDB = new ArrayList<>(); //데이터를 저장할 임시DB
-    private  Long nextId = 1L; //고유번호 업데이트
-    public Article addArticle(Article article){
+    private final ArticleRepository articleRepository;
 
-        if (article.getAuthor() == null
-                || article.getPassword() == null
-                || article.getTitle() == null
-                || article.getContent() == null
-        ) {
-            throw new IllegalArgumentException("제목, 내용, 작성자, 비밀번호는 필수 입력 항목입니다.");
-        }
-        //Article 객체 생성
-        Article newArticle = new Article(
-                article.getContent(),
-                nextId++,
-                article.getTitle(),
-                article.getAuthor(),
-                article.getPassword()
-        );
+    public ArticleResponse addArticle(AddArticleRequest request){
+        //request 객체의 .toEntity()를 통해 Article 객체 생성
+        Article article=request.toEntity();
 
-        //DB에 객체 저장
-        articleDB.add(newArticle);
+        //Article객체를 JPA의 save() 를 사용하여 DB에 저장
+        articleRepository.save(article);
 
-        return newArticle;
+        // article 객체를 response DTO 생성하여 반환
+        //  response 클래스의 정작 팩토리 메서드 of() 통해 API 응답객체 생성
+        return ArticleResponse.of(article);
+    }
+
+    public List<SimpleArticleResponse> getAllArticles(){
+        List<Article> articleList=articleRepository.findAll();
+        List<SimpleArticleResponse> articleResponseList = articleList.stream()
+                .map(article -> SimpleArticleResponse.of(article))
+                .toList();
+        return articleResponseList;
 
     }
 
-    public List<Article> findAll() {
-        return articleDB;
-    }
-
-    public Article findById(long id) {
-        for (Article article : articleDB) {
-            if (article.getId().equals(id)) {
-                return article;
-            }
-        }
-        throw new NoSuchElementException("해당 id 게시물을 찾을 수 없습니다");
+    public ArticleResponse getArticle(Long id){
+        Article article=articleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("해당 ID의 게시글을 찾을 수 없습니다: " + id));
+        return ArticleResponse.of(article);
     }
 }
+
+
